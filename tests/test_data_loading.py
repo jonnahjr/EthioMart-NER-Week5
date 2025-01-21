348bbf87c4413bc8b37538542b3b2021e9c21d48
yonas bogale <pronivamusic14@gmail.com>
2025-01-21 21:09:36 +0300

    Unit test for the 'load_dataset' function from the 'src.data_loading' module to verify that the dataset is not empty when loaded

--- tests/test_data_loading.py
+++ tests/test_data_loading.py
@@ -3,7 +3,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
   
 import unittest 
-from src.data_loading import load_dataset 
+from src.data_loading import load_dataset  
 
 class TestDataLoading(unittest.TestCase):
     def test_load_dataset(self):
